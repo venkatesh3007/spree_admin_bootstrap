823964a3aeab29864b5417a3993d8b6bb6c37550
Willian <willian@pythonheads.nl>
2013-03-12 14:05:19 +0100

    Removed frontend css and js

--- lib/generators/spree_admin_bootstrap/install/install_generator.rb
+++ lib/generators/spree_admin_bootstrap/install/install_generator.rb
@@ -3,12 +3,12 @@ module SpreeAdminBootstrap
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_admin_bootstrap\n"
+        #append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_admin_bootstrap\n"
         append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_admin_bootstrap\n"
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_admin_bootstrap\n", :before => /\*\//, :verbose => true
+        #inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_admin_bootstrap\n", :before => /\*\//, :verbose => true
         inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_admin_bootstrap\n", :before => /\*\//, :verbose => true
       end
 
