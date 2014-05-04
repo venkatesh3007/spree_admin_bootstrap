678e00ec5b7af55092ec0f6d2e470b1e16373169
venkatesh3007 <venkatesh3007@gmail.com>
2014-05-05 05:24:52 +0530

    Update install_generator.rb

--- lib/generators/spree_admin_bootstrap/install/install_generator.rb
+++ lib/generators/spree_admin_bootstrap/install/install_generator.rb
@@ -1,12 +1,15 @@
 module SpreeAdminBootstrap
   module Generators
     class InstallGenerator < Rails::Generators::Base
-
+  
+      source_root File.expand_path("../templates", __FILE__)
+      
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require admin/spree_admin_bootstrap\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_admin_bootstrap\n"
       end
 
       def add_stylesheets
+        copy_file 'stylesheets/spree_admin_bootstrap.css', 'app/assets/stylesheets/spree/backend/spree_admin_bootstrap.css'
         remove_file 'vendor/assets/stylesheets/spree/backend/all.css'
         create_file 'vendor/assets/stylesheets/spree/backend/all.css' do
           "/*
@@ -14,7 +17,9 @@ module SpreeAdminBootstrap
           * and any sub-directories. You're free to add application-wide styles to this file and they'll appear at
           * the top of the compiled file, but it's generally better to create a new file per style scope.
           *
-          *= require admin/spree_admin_bootstrap
+          *= require spree/backend/spree_admin_bootstrap
+          *= require_self
+          *= require_tree .
           */
           "
         end
